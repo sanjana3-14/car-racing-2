b25c7256bbc959f0091826568ac251a927868dd4
sanjana3-14 <73714604+sanjana3-14@users.noreply.github.com>
2021-01-22 17:52:02 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var database;
+var gameState = 0;
+var playerCount = 0;
+var form,player,game
+var allPlayers
+var distance = 0
+
+var car1,car2,car3,car4,cars
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(displayWidth-20,displayHeight-30);
+
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw(){
+  background("white");
+  
+    if (playerCount === 4){
+      game.update(1)
+    }
+    if (gameState === 1){
+      clear();
+      game.play();
+    }
+  
+}
+
+
